04efe16cc38ef9b96502fd4f9f5d8731b3a4a535
zuri1 <zuri.biringer@gmail.com>
2015-01-17 12:14:01 -0800

    Temporarily move networking logic to FirstViewController

--- VictoriousWeather/FirstViewController.m
+++ VictoriousWeather/FirstViewController.m
@@ -8,7 +8,7 @@
 
 #import "FirstViewController.h"
 
-@interface FirstViewController ()
+@interface FirstViewController () <NSURLSessionDownloadDelegate>
 
 @end
 
@@ -17,6 +17,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSURL *downloadURL = [NSURL URLWithString:@"http://www.myweather2.com/developer/forecast.ashx?uac=KDrRbvwbAt&output=json&query=97006&temp_unit=f"];
+    [self downloadDataFromURL:downloadURL withCompletionHandler:^(NSData *data) {
+        if (data != nil) {
+            
+            NSError *error;
+            NSDictionary *weatherDict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+            
+            if (error != nil) {
+                NSLog(@"%@", [error localizedDescription]);
+            } else {
+                NSLog(@"%@", weatherDict);
+            }
+        }
+    }];
+}
+
+- (void)downloadDataFromURL:(NSURL *)url withCompletionHandler:(void (^)(NSData *data))completionHandler {
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
+    
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+        } else {
+            NSInteger HTTPStatusCode = [(NSHTTPURLResponse *)response statusCode];
+            NSLog(@"HTTP status code %ld", (long)HTTPStatusCode);
+        }
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            completionHandler(data);
+        }];
+    }];
+    [dataTask resume];
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location {
+    
 }
 
 - (void)didReceiveMemoryWarning {
